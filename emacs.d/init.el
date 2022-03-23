fe29c3d1989bf67f81bda0bd9e37cadfddfa89ac
matias685 <78141961+matias685@users.noreply.github.com>
2022-03-23 20:21:08 +0200

    Add files via upload

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,19 +1,28 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (toggle-scroll-bar -1) 
+;; Case insensitive completion
+(setq completion-ignore-case t)
+(setq read-file-name-completion-ignore-case t)
+(setq completion-cycle-threshold t)
+(setq make-backup-files nil) ; stop creating ~ files
+;; interactive mode
+(ido-mode 1)
+(setq ido-everywhere t)
+(setq ido-enable-flex-matching t)
+(setq ido-use-filename-at-point 'guess)
+(ido-grid-mode 1)
+;; Focus on the new frame
+(defadvice split-window (after split-window-after activate)
+  (other-window 1))
+;; Enable line numbers
 (global-display-line-numbers-mode 1)
+;; Default font
 (add-to-list 'default-frame-alist
              '(font . "Iosevka 12"))
-;; This is only needed once, near the top of the file
-(eval-when-compile
-  ;; Following line is not needed if use-package.el is in ~/.emacs.d
-  (add-to-list 'load-path "<path where use-package is installed>")
-  (require 'use-package))
+(require 'use-package)
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -21,14 +30,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("0d01e1e300fcafa34ba35d5cf0a21b3b23bc4053d388e352ae6a901994597ab1" "da186cce19b5aed3f6a2316845583dbee76aea9255ea0da857d1c058ff003546" default))
- '(package-selected-packages '(doom-themes doom-modeline dashboard)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+   '("0d01e1e300fcafa34ba35d5cf0a21b3b23bc4053d388e352ae6a901994597ab1" default))
+ '(package-selected-packages
+   '(ido-grid-mode use-package rainbow-mode org kaolin-themes evil emojify doom-themes doom-modeline dashboard)))
 (use-package dashboard
   :ensure t
   :config
@@ -37,13 +41,28 @@
   (setq dashboard-set-heading-icons t)
   (setq dashboard-startup-banner 'logo)
   (dashboard-setup-startup-hook))
-;; Set the banner
-;;(setq dashboard-startup-banner [official])
 (when (display-graphic-p)
   (require 'all-the-icons))
-(require 'doom-modeline)
-(doom-modeline-mode 1)
-(use-package doom-themes
-	     :ensure t
-	     :config
-	     (load-theme 'doom-vibrant))
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+(require 'evil)
+(evil-mode 1)
+;;(use-package doom-themes
+;;	     :ensure t
+;;	     :config
+;;	     (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+;;              doom-themes-enable-italic t) ; if nil, italics is universally disabled
+;;	     (load-theme 'doom-vibrant))
+(use-package kaolin-themes
+  :config
+  (load-theme 'kaolin-aurora t)
+  (kaolin-treemacs-theme))
+(use-package emojify
+  :hook (after-init . global-emojify-mode))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
