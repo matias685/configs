2c88cde06afaa9db13b7b47db2ee09f8e374e5df
matias685 <78141961+matias685@users.noreply.github.com>
2022-09-03 15:05:21 +0300

    Add files via upload

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,13 +2,18 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin', '~/.config/nvim/plugged')
 Plug 'junegunn/goyo.vim'
 Plug 'lilydjwg/colorizer'
-Plug 'feline-nvim/feline.nvim'
+-- Plug 'feline-nvim/feline.nvim'
+Plug 'windwp/nvim-autopairs'
+Plug 'nvim-lualine/lualine.nvim'
 Plug 'kyazdani42/nvim-web-devicons'
+-- Plug 'navarasu/onedark.nvim'
+Plug 'aktersnurra/no-clown-fiesta.nvim'
 vim.call('plug#end')
 
 vim.opt.hidden = true
 vim.opt.swapfile = false
 vim.opt.foldenable = false
+vim.opt.hlsearch = false
 
 vim.opt.smartindent = true
 vim.opt.expandtab = true
@@ -34,7 +39,50 @@ vim.opt.termguicolors = true
 vim.mapleader = ' '
 vim.g.mapleader = ' '
 
-require('feline').setup()
+-- status bar
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
+
+require("no-clown-fiesta").setup({ type = "bold,italic" })
+require("nvim-autopairs").setup {}
 
 vim.cmd('au BufReadPost *.zsh,.zshrc set filetype=sh')
 vim.cmd('au BufReadPost *.conf set filetype=config')
